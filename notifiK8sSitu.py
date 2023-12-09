f9b85544a8450ac7868249bd845ebff9b8ac5d8c
yasuaki sera <yasuaki.sera15@gmail.com>
2023-12-09 14:18:15 +0900

    parse response

--- notifiK8sSitu.py
+++ notifiK8sSitu.py
@@ -3,7 +3,12 @@ import requests
 
 # 一旦URLべた書き
 # kubectl proxy使用中に実行
-r = requests.get('http://127.0.0.1:8001/api/v1/namespaces')
-print(r.text)
+res = requests.get('http://127.0.0.1:8001/api/v1/namespaces')
+namespaces = res.json()["items"]
+
+for ns in namespaces:
+    print(ns["metadata"]["name"])
+print('----')
+print('namespaces: ',len(namespaces))
 
 
