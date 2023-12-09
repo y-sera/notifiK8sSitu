147c282f073d524c62f405a09cde682ea1e524f2
yasuaki sera <yasuaki.sera15@gmail.com>
2023-12-09 19:58:35 +0900

    add slack notification

--- notifiK8sSitu.py
+++ notifiK8sSitu.py
@@ -1,14 +1,26 @@
 #!/usr/bin/python3
 import requests
+import json
+import os
 
 # 一旦URLべた書き
 # kubectl proxy使用中に実行
 res = requests.get('http://127.0.0.1:8001/api/v1/namespaces')
 namespaces = res.json()["items"]
 
+notice_text = 'namespaces: ' + str(len(namespaces)) + '\n'
+notice_text += '------' + '\n'
+print('namespaces: ',len(namespaces))
+print('----')
+
 for ns in namespaces:
     print(ns["metadata"]["name"])
-print('----')
-print('namespaces: ',len(namespaces))
+    notice_text += ns["metadata"]["name"] + '\n'
 
 
+
+webHookUrl = os.environ['SLACK_WEBHOOK_URL']
+requests.post(webHookUrl, data = json.dumps({
+  'text': notice_text,
+  'username': u'namespace count'
+}))
